0f34b2eeb6f7249db3b2804a166b5190fd46074d
Mobyfab <mobyfab@free.fr>
2014-04-29 00:51:03 +0200

    [GUI] Updated CBR600RR data struct

--- GUI/MotoLink/inc/datastructures.h
+++ GUI/MotoLink/inc/datastructures.h
@@ -3,12 +3,28 @@
 
 /* This will be based on the HRC tool hex files */
 
-struct cbr600rr_t {
+#define CBR600RR07_SIGN "07CBR600KEA6KO"
 
-    char idle;
-    char pit_limiter;
-    char tps_row[11];
-    char rpm_row[16];
+struct cbr600rr_map_t {
+
+    /* All RPM values are divided by 100 to fit in 8 bits */
+    char ign[4*16];     /* Ignition table */
+    char dummy1[16];    /* Unknown - zeros */
+    char ign[7*16];     /* Ignition table */
+    char fuel[7*16];    /* Fuel table */
+    char dummy2[16];    /* Unknown - zeros */
+    char fuel[4*16];    /* Fuel table */
+    char rpm_row[16];   /* RPM row values */
+    char tps_row[11];   /* TPS row values */
+    char dummy3;        /* Unknown */
+    char staging[16];   /* Staging table */
+    char pit_limiter;   /* Pit limiter value */
+    char dummy4;        /* Unknown - zeros */
+    char shift_light;   /* Shift light value */
+    char dummy5;        /* Unknown - zeros */
+    char idle;          /* Idle value */
+    char dummy6;        /* Unknown - zeros */
+    char signature[14]; /* "07CBR600KEA6KO" */
 };
 
 
