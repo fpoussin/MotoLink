19cae63719646198262a59c9d70a4b848b86bdb8
Fabien Poussin <fabien.poussin@gmail.com>
2017-04-23 02:50:35 +0200

    [GUI] Fixed tables headers values.

--- GUI/src/tablemodel.cpp
+++ GUI/src/tablemodel.cpp
@@ -14,7 +14,6 @@ TableModel::TableModel(QUndoStack *stack, int min, int max, int def, bool single
     mPermanent = permanent;
     mSinglerow = singlerow;
     this->mId = 0;
-    this->fill(false);
 
     const int defaultRpm[] = {0, 2000, 4000, 5500, 7000, 8000, 9000,
                   10000, 11000, 12000, 13000, 14000, 15000, 16000,
@@ -31,6 +30,8 @@ TableModel::TableModel(QUndoStack *stack, int min, int max, int def, bool single
     {
         mDefaultTps.append(defaultTps[i]);
     }
+
+    this->fill(false);
 }
 
 TableModel::~TableModel()
